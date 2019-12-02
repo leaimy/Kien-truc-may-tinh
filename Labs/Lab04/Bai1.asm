7989320cb4fd2fbd17101264573e03beddb606a4
leaimy <hantctk42@gmail.com>
2019-12-02 22:10:09 +0700

    Bo sung bai01

---
+++ Labs/Lab04/Bai1.asm
@@ -0,0 +1,42 @@
+.MODEL SMALL
+.STACK 100h
+.DATA
+   MSG DB 13,10,'NHAP CHUOI KY TU:$'
+   MSG1 DB 13,10,'CHUOI NGUOC:$'
+.CODE
+   MOV AX,@DATA
+   MOV DS,AX
+   
+   MOV AH,9h
+   LEA DX,MSG
+   INT 21h 
+   XOR CX,CX  
+ 
+INPUT:   
+   MOV AH,1
+   INT 21h
+   
+   CMP AL,0Dh
+   JE BREAK 
+
+   XOR AH,AH
+   PUSH AX   
+   INC CX
+   JMP INPUT
+   
+BREAK:
+   ; XUAT CHUOI DAO NGUOC
+   MOV AH, 9
+   LEA DX, MSG1
+   INT 21h
+   
+PRINT:
+   MOV AH, 2
+   POP DX
+   INT 21h
+   LOOP PRINT
+   
+   ; Thoat chuong trinh
+   MOV AH,4Ch
+   INT 21h
+END
