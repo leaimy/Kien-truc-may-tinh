1eb4b9a6a1f321e19a48b582479955df40febed2
leaimy <hantctk42@gmail.com>
2019-12-02 22:20:20 +0700

    NTH yeu NTH

--- Labs/Lab04/Bai1.asm
+++ Labs/Lab04/Bai1.asm
@@ -7,36 +7,41 @@
    MOV AX,@DATA
    MOV DS,AX
    
-   MOV AH,9h
-   LEA DX,MSG
-   INT 21h 
-   XOR CX,CX  
- 
-INPUT:   
-   MOV AH,1
-   INT 21h
-   
-   CMP AL,0Dh
-   JE BREAK 
-
-   XOR AH,AH
-   PUSH AX   
-   INC CX
-   JMP INPUT
-   
-BREAK:
-   ; XUAT CHUOI DAO NGUOC
-   MOV AH, 9
-   LEA DX, MSG1
-   INT 21h
-   
-PRINT:
-   MOV AH, 2
-   POP DX
-   INT 21h
-   LOOP PRINT
+   CALL NTH_YEU_NTH
    
    ; Thoat chuong trinh
    MOV AH,4Ch
    INT 21h
+   
+   NTH_YEU_NTH PROC
+       MOV AH,9h
+       LEA DX,MSG
+       INT 21h 
+       XOR CX,CX  
+     
+    INPUT:   
+       MOV AH,1
+       INT 21h
+       
+       CMP AL,0Dh
+       JE BREAK 
+    
+       XOR AH,AH
+       PUSH AX   
+       INC CX
+       JMP INPUT
+       
+    BREAK:
+       ; XUAT CHUOI DAO NGUOC
+       MOV AH, 9
+       LEA DX, MSG1
+       INT 21h
+       
+    PRINT:
+       MOV AH, 2
+       POP DX
+       INT 21h
+       LOOP PRINT   
+    RET
+   NTH_YEU_NTH ENDP
 END
